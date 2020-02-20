256bea17f1d100b647611926a371fa35bab033db
MarggythG <johanna98g@gmail.com>
2020-02-19 21:17:02 -0500

    19Febr

--- Ejercicio1/Assets/Scripts/PlayerController.cs
+++ Ejercicio1/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,16 +9,32 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
     public float speed;
-     private Vector3 position;
+    private Vector3 position;
+    private Vector3 posicion;
+
+    public Transform particulas;
+    private ParticleSystem sistemaParticulas;
+    private AudioSource audioRecoleccion;
+
+    private int cubes = 12;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        sistemaParticulas = particulas.GetComponent<ParticleSystem>();
+        sistemaParticulas.Stop();
+        audioRecoleccion = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (cubes == 0)
+        {
+            // this.transform.position = new Vector3(0,1,0);
+            // cubes = 1;
+            SceneManager.LoadScene(1);
+        }
     }
 
     void FixedUpdate()
@@ -28,11 +45,22 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other) {
-        if(other.gameObject.CompareTag("Recolectable")){
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Recolectable"))
+        {
             //other.gameObject.SetActive(false);
+            posicion = other.gameObject.transform.position;
+            particulas.position = posicion;
+            sistemaParticulas.Play();
+            cubes -= 1;
+            audioRecoleccion.Play();
             Destroy(other.gameObject);
-        }else{
+            Debug.Log(cubes);
+
+        }
+        else
+        {
 
         }
     }
